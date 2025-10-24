76076c7888702349d105dc7aafed1166571bbf8d
Pratyush <79155396+Mpratyush54@users.noreply.github.com>
2025-10-24 19:01:40 +0530

    feat: added the saving api for saving data of users to DB

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+// Imports
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+
+// dot env config
+dotenv.config({ path: './.env' })
+// express config
+app.use(bodyParser.json());
+
+
+// Cors Setup
+app.use(function(req, res, next) {
+    console.log(req.headers.origin);
+    console.log(req.headers.referer);
+    // if (req.headers.origin == 'http://localhost:4200' || req.headers.referer == 'http://localhost:4200/') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin , X-Requested-With, Content-Type, Accept,x-username, x-email, x-token');
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); // ✅ allow cookies/auth
+
+    next();
+    // } else {
+    //     return res.sendStatus(401)
+    // }
+})
+// api setup
